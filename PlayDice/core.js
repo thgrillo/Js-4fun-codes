117e61aebde01e029e71f4633e057bbceb668551
Thiago Grillo <thiago_grillo@hotmail.com>
2025-02-08 00:13:15 -0300

    Update core.js

--- PlayDice/core.js
+++ PlayDice/core.js
@@ -8,36 +8,3 @@ function jogarDado(){
     result.innerHTML = `Dado de ${numdado} lados jogado: ${sorteio}.`
     
 }
-
-/*
-TESTE CLASSE
-
-function jogarDado(){
-    class Dice {
-        constructor(sides, result) {
-            this.sides = sides
-            this.result = result
-        }
-        getSides() {
-            var sides = document.getElementById('txtnumdado').value
-            sides = this.sides
-            return this.sides, this.playDice()
-        }
-        playDice() {
-            var play = Math.floor(Math.random() * this.sides) + 1
-            play = this.result
-            return this.result, this.statusShow()
-        }
-        statusShow() {
-            var show = window.document.getElementById('res')
-            show.style.textAlign = 'center'
-            show.style.color = 'red'
-            show.innerHTML = `Dado de ${this.sides} lados jogado: ${this.result}.`
-        }
-        
-    }
-    let myDice = new Dice(sides)
-    // myDice.getSides()
-    console.log(myDice)
-}
-*/
